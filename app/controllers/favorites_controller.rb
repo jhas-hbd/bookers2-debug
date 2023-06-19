4ecc06256a75e61215ece5945271bd21fc089073
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-06-19 00:47:51 +0000

    [Update]favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,14 @@
 class FavoritesController < ApplicationController
 
-def create
-  session[:previous_url] = request.referer
-  book = Book.find(params[:book_id])
-  favorite = current_user.favorites.new(book_id: book.id)
-  favorite.save
-  redirect_to session[:previous_url]
-end
+  def create
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite.save
+  end
 
-def destroy
-  session[:previous_url] = request.referer
-  book = Book.find(params[:book_id])
-  favorite = current_user.favorites.find_by(book_id: book.id)
-  favorite.destroy
-  redirect_to session[:previous_url]
-end
+  def destroy
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite.destroy
+  end
 end
