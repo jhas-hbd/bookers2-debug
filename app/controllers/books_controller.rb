01fd029cb7aeac8e7c74eec9a90857c4a2b6a696
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-07-01 23:16:39 +0000

    [Add]task9d-controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :stars)
+    params.require(:book).permit(:title, :body, :stars, :category)
   end
 
   def is_matching_login_user
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,7 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
 
   scope :latest, -> {order(created_at: :desc)}
   scope :star_count, -> {order(stars: :desc)}
