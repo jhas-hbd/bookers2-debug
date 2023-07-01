957aeeca4b95d9679bb02d31277f87befd7589bf
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-07-01 22:08:35 +0000

    {Start]task8d

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,10 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  scope :latest, -> {order(created_at: :desc)}
+
+  scope :star_count, -> {order(stars: :desc)}
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
