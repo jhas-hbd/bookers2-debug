6a1e4614a679cec2863825b805a1c05364933154
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-07-02 00:18:49 +0000

    [Add]task9d-view

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+  validates :category, presence: true
 
   scope :latest, -> {order(created_at: :desc)}
   scope :star_count, -> {order(stars: :desc)}
