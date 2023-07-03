97b65a7d844dfc70f7393314a4c6b6d0c0400233
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-07-03 07:00:45 +0000

    [Add]task7c-model/controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_many :follower_user, through: :followed, source: :follower
   has_many :following_user, through: :follower, source: :followed
 
+  has_many :group_users, dependent: :destroy
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
