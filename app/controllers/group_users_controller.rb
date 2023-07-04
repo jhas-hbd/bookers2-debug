0ce947de601891ea3e633c319fed5ae8b23d55a1
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-07-04 04:17:31 +0000

    [Start]task9c

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -4,7 +4,7 @@ class GroupUsersController < ApplicationController
     group_user.save
     redirect_to request.referer
   end
-  
+
   def destroy
     group_user = current_user.group_users.find_by(group_id: params[:group_id])
     group_user.destroy
